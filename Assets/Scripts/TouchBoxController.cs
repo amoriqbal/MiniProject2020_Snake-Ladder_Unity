f08e2b9007a1f56628d5420e8f316d6a54096411
amoriqbal <43443370+amoriqbal@users.noreply.github.com>
2020-01-19 01:14:09 -0800

    modularity achieved by using the waypoints of pieces for positioning touch boxes

--- Assets/Scripts/TouchBoxController.cs
+++ Assets/Scripts/TouchBoxController.cs
@@ -15,21 +15,26 @@ public class TouchBoxController : MonoBehaviour
 
     public GameObject[] TouchableTiles=new GameObject[BoardWaypoints._NUM_BOXES];
     public GameObject TouchableTilePrefab;
-    public Vector3 diff;
-    public Vector3 Pos1;
+    //public Vector3 diff;
+    //public Vector3 Pos1;
+    private Vector3 offset;
     void Start()
     {
-        TouchableTiles[0]=GameObject.Instantiate(TouchableTilePrefab);
-        TouchableTiles[0].transform.position = Pos1;
+        //TouchableTiles[0]=GameObject.Instantiate(TouchableTilePrefab);
+        //TouchableTiles[0].transform.position = Pos1;
+        offset = TouchableTilePrefab.transform.position - BoardWaypoints.Instance.waypoints[0];
         for (int i = 1; i < BoardWaypoints._NUM_BOXES; i++)
         {
             TouchableTiles[i] = GameObject.Instantiate(TouchableTilePrefab);
-            if (i % BoardWaypoints._NUM_COLUMNS == 0)
+            TouchableTiles[i].transform.position = BoardWaypoints.Instance.waypoints[i] + offset;
+            /*if (i % BoardWaypoints._NUM_COLUMNS == 0)
                 TouchableTiles[i].transform.position = new Vector3(TouchableTiles[i - 1].transform.position.x, TouchableTiles[i - 1].transform.position.y + diff.y, 0f);
             else if ((i / BoardWaypoints._NUM_COLUMNS) % 2 == 0)
                 TouchableTiles[i].transform.position = new Vector3(TouchableTiles[i - 1].transform.position.x + diff.x, TouchableTiles[i - 1].transform.position.y, 0f);
             else
                 TouchableTiles[i].transform.position = new Vector3(TouchableTiles[i - 1].transform.position.x - diff.x, TouchableTiles[i - 1].transform.position.y, 0f);
+            */
+
         }
     }
 
