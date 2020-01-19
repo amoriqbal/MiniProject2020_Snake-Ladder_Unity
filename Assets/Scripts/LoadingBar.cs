2663f92f94655ed663687d122c228a6be85f1051
amoriqbal <43443370+amoriqbal@users.noreply.github.com>
2020-01-19 06:04:25 -0800

    scene loader transition scenes added

---
+++ Assets/Scripts/LoadingBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingBar : MonoBehaviour
+{
+    public Image loadbar;
+    public string sceneToLoad;
+    private void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+    IEnumerator LoadSceneAsync()
+    {
+        yield return null;
+        AsyncOperation loadAsyncOperation=SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!loadAsyncOperation.isDone)
+        {
+            loadbar.fillAmount = loadAsyncOperation.progress;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
