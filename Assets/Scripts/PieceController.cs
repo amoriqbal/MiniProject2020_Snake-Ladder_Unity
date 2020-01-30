e8e08f141df13caf70abba59e4d5c90450e97245
amoriqbal <43443370+amoriqbal@users.noreply.github.com>
2020-01-30 06:02:24 -0800

    ladder animattion complete. debuged the ladder system.

--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PieceController : MonoBehaviour
 {
-    public static bool pieceMoving;
+    private bool _pieceMoving;
+    public bool pieceMoving
+    {
+        get
+        {
+            return _pieceMoving;
+        }
+        set
+        {
+            _pieceMoving = value;
+            Debug.Log("piecemoving :" + value.ToString());
+        }
+    }
+    public const int ladderAnimFrames=10;
     public int pos;//initial position of the piece
     public Vector3 offset;
 
@@ -16,7 +30,10 @@ public class PieceController : MonoBehaviour
 
     public void JumpTo(int i)
     {
-        
+        if(i<0)
+        {
+            Debug.LogError("Jumpto negative command:" + i);
+        }
         if (i < BoardWaypoints._NUM_BOXES-1)
         {
             pos = i;
@@ -45,12 +62,9 @@ public class PieceController : MonoBehaviour
         while (pieceMoving)
             yield return new WaitForSeconds(0.1f);
         pieceMoving = true;
-        while (i < BoardWaypoints._NUM_BOXES-1)
+        while (i < BoardWaypoints._NUM_BOXES-1&&i>=0)
         {
-            if (pos >= BoardWaypoints._NUM_BOXES - 1||pos<0)
-            {
-                JumpTo(i);
-            }
+            Debug.Log("transitto function running");
             while (pos != i)
             {
                 if (pos > i)
@@ -61,7 +75,17 @@ public class PieceController : MonoBehaviour
             }
             if (BoardWaypoints.Instance.snl[pos] != -1 && BoardWaypoints.Instance.snl[pos] != pos)
             {
+                //i = BoardWaypoints.Instance.snl[pos];
+
+                //snake or ladder encountered
+                pieceMoving = false;
                 i = BoardWaypoints.Instance.snl[pos];
+                SnlTransition(pos);
+                while (pieceMoving)
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+                pieceMoving = true;
             }
             else
             {
@@ -97,6 +121,77 @@ public class PieceController : MonoBehaviour
     public IEnumerator _Transit(int i)
     {
         yield return TransitTo(pos + i);
+    }
+
+    public void SnlTransition(int i)
+    {
+        StartCoroutine(_SnlTransition(i));
+    }
+
+    IEnumerator _SnlTransition(int i)
+    {
+        while(pieceMoving)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        pieceMoving = true;
+        
+        if(BoardWaypoints.Instance.snl[i]<i)
+        {
+            //snake
+            if(i>=BoardWaypoints._NUM_BOXES||i<0)
+            {
+                Exception e = new Exception("snl data corrupted");
+                throw e;
+            }
+            JumpTo(BoardWaypoints.Instance.snl[i]);
+            
+        }
+
+        else if(BoardWaypoints.Instance.snl[i]>i)
+        {
+            //ladder
+            if (i >= BoardWaypoints._NUM_BOXES || i < 0)
+            {
+                Exception e = new Exception("snl data corrupted");
+                throw e;
+            }
+            pieceMoving = false;
+            LadderAnimation(BoardWaypoints.Instance.snl[i]);
+            while (pieceMoving)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            pieceMoving = true;
+        }
+
+        pieceMoving = false;
+    }
+
+    public void LadderAnimation(int i)
+    {
+        StartCoroutine(_LadderAnimaton(i));
+    }
+
+    IEnumerator _LadderAnimaton(int i)
+    {
+        while (pieceMoving)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+        pieceMoving = true;
+
+        Vector3 d = (BoardWaypoints.Instance.waypoints[i] + offset - transform.position)/ladderAnimFrames;
+        for(int count=1;count<=ladderAnimFrames;count++)
+        {
+            transform.position = transform.position + d;
+            yield return new WaitForSeconds(0.1f);
+        }
+        pieceMoving = false;
+        Debug.Log("ladder anim finished");
+        //pos = i;
+        JumpTo(i);
     }
 
 }
